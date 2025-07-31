bc2e0ce12be4f0aa519eb403efad60662fa18614
Erisolo <marsolor@ucm.es>
2025-07-31 20:12:33 +0200

    assets movidos y tercer minijuego creado

---
+++ Kyklia/Assets/scripts/minigames/atropos/scissorsController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scissorsController : MonoBehaviour
+{
+    [SerializeField]
+    Transform dest;
+    [SerializeField] float timeTo;
+    bool inDest = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeanTween.move(gameObject, new Vector2(transform.position.x, dest.position.y), timeTo).setOnComplete(tweenFinish);
+    }
+
+    void tweenFinish()
+    {
+        inDest = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inDest)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("finished");
+            }
+        }
+    }
+}
